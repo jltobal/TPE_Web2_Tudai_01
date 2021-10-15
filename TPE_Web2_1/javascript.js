0ffca87303bf72e0582e805b9994e9b229a78828
joaquin <ziba.tobal@gmail.com>
2021-10-14 21:06:48 -0300

    correcion de console.log

--- TPE_Web2_1/javascript.js
+++ TPE_Web2_1/javascript.js
@@ -5,13 +5,11 @@ select.addEventListener("change", seleccionarMetodo);
 async function seleccionarMetodo() {
     event.preventDefault();
     let metodo = select.value
-    console.log(metodo);
     try {
         let respuesta = await fetch(`http://localhost/proyectos/WEB-2/PHP/TP_Especial_1/TPE_Web2_1/filtrado/${metodo}`);
         if (respuesta.ok) {
             let html = await respuesta.text();
             document.querySelector("#ajax-contenedor").innerHTML = html;
-            console.log(html);
         } else {
             document.querySelector("#ajax-contenedor").innerHTML = "Fallo URL";
         }
